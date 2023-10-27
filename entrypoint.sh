54527cb4ef9d1eb6c16a3acefe8fa67bc30fb3d5
Rakshit Kumar <32777813+mighty-phoenix@users.noreply.github.com>
2023-10-27 07:13:30 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,7 @@ export REACT_APP_API_HOST=${REACT_APP_API_HOST}
 export REACT_APP_CLIENT_HOST=${REACT_APP_CLIENT_HOST}
 export REACT_APP_ONE_ACCOUNT_EXTERNAL_ID=${REACT_APP_ONE_ACCOUNT_EXTERNAL_ID}
 
-cd server
-NODE_ENV=production nohup node index.js &
-
-cd ../client
+cd client
 mkdir -p dist
 
 # Build the UI in the background
